612be4f0b58055d717bac6323e314e4568ae6c70
Simon Chong <simons.mail@gmail.com>
2015-11-09 08:28:49 +1100

    extra test for link injection

--- server/controllers/assets/injectLinks_test.go
+++ server/controllers/assets/injectLinks_test.go
@@ -3,7 +3,10 @@ package assets
 import (
 	"net/http"
 	"net/url"
+	"regexp"
 	"testing"
+
+	"github.com/simonchong/linny/server/paths"
 )
 
 var mockReq = &http.Request{URL: &url.URL{Host: "TestHost", Path: "TestPath/"}, Host: "TestHost"}
@@ -21,5 +24,17 @@ func TestInjectLinksILK(t *testing.T) {
 	var res = InjectLinks("test", html, mockReq)
 	if res != htmlRes {
 		t.Errorf("Expected '%s' got '%s'", htmlRes, res)
+	}
+}
+
+func TestInjectLinksMLK(t *testing.T) {
+	html := `test <a href="{{mlk 'somePage.html'}}"></a> test`
+	var res = InjectLinks("testAID", html, mockReq)
+	reg := `^test <a href="//TestHost/` + paths.MeasureDir + `/k\?g=\d+&a=testAID&l=%2F%2FTestHost%2FTestPath%2FsomePage.html"></a> test$`
+	matched, err := regexp.MatchString(reg, res)
+
+	// log.Println(reg)
+	if !matched || err != nil {
+		t.Errorf("Not in the expected format: '%s'", res)
 	}
 }
